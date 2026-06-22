4d9157f8495a3beaee21c25cfc06e5aa94a5e3eb
CodeSync <codesync@users.noreply.github.com>
2026-06-22 22:49:42 +0530

    Solved: ENG (LintCode) Sheets: None

--- CodeSync/LintCode/all/ENG/solution.java
+++ CodeSync/LintCode/all/ENG/solution.java
@@ -7,15 +7,13 @@
  * Topics: Linked ListSimulation, Linked List, Simulation, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-22T17:00:58.849Z
+ * Synced: 2026-06-22T17:01:45.502Z
  */
 
-public·class·Solution·{
-····public·ListNode·addLists(ListNode·l1,·ListNode·l2)·{
 ········Stack<Integer>s1=new·Stack<>();
 ········Stack<Integer>s2=new·Stack<>();
 ········while(l1!=null){
-············s1.push(li.val);
+············s1.push(l1.val);
 ············l1=l1.next;
 ········}
 ········while(l2!=null){
@@ -25,14 +23,6 @@ public·class·Solution·{
 ········int·carry=0;
 ·······ListNode·head=null;
 ·······while(!s1.isEmpty()·||·!s2.isEmpty()||·carry=0){
+····public·ListNode·addLists(ListNode·l1,·ListNode·l2)·{
 ···········int·sum=carry;
 ···········if(!s1.isEmpty())sum+=s1.pop();
-···········if(!s2.isEmpty())sum+=s2.pop();
-···········carry=sum/10;
-···········ListNode·node=new·ListNode(sum·%·10);
-·······}
-
-······
-····}
-···········node.next=head;
-}
