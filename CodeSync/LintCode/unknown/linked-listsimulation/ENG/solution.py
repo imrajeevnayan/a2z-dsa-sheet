736e1e8335b7093d94fb97566c2b2488511c375c
CodeSync <codesync@users.noreply.github.com>
2026-06-22 14:38:04 +0530

    Solved: ENG (LintCode) Sheets: None

--- CodeSync/LintCode/unknown/linked-listsimulation/ENG/solution.py
+++ CodeSync/LintCode/unknown/linked-listsimulation/ENG/solution.py
@@ -6,13 +6,19 @@
 # Topics: Linked ListSimulation, Linked List, Simulation, Test Data Test Output
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-06-22T09:06:30.280Z
+# Synced: 2026-06-22T09:08:03.458Z
 
 public·class·Solution·{
 ····public·ListNode·addLists(ListNode·l1,·ListNode·l2)·{
 ······
-····}
 ········Stack<Integer>s1=new·Stack<>();
 ········Stack<Integer>s2=new·Stack<>();
-········
+········while(l1!=null){
+············s1.push(li.val);
+········}
+············l1=l1.next;
+········while(l2!=null){
+············s2.push(l2.val)
+········}
+····}
 }
