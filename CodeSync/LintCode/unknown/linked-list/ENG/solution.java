2381ff3ab7c8fef0328903ec699a5ff0caa6b80f
CodeSync <codesync@users.noreply.github.com>
2026-06-22 22:50:14 +0530

    Solved: ENG (LintCode) Sheets: None

--- CodeSync/LintCode/unknown/linked-list/ENG/solution.java
+++ CodeSync/LintCode/unknown/linked-list/ENG/solution.java
@@ -7,21 +7,20 @@
  * Topics: Linked ListSimulation, Linked List, Simulation, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-22T17:02:35.032Z
+ * Synced: 2026-06-22T17:10:22.562Z
  */
 
-········int·carry=0;
-·······ListNode·head=null;
-·······while(!s1.isEmpty()·||·!s2.isEmpty()||·carry=0){
-···········int·sum=carry;
-···········if(!s1.isEmpty())sum+=s1.pop();
-···········if(!s2.isEmpty())sum+=s2.pop();
-···········carry=sum/10;
-···········ListNode·node=new·ListNode(sum·%·10);
-···········node.next=head;
-···········head=node;
-·······}
-·····return·head;
-······
+············if·(!s2.isEmpty())
+················sum·+=·s2.pop();
+
+············carry·=·sum·/·10;
+
+············ListNode·node·=·new·ListNode(sum·%·10);
+
+············node.next·=·head;
+············head·=·node;
+········}
+
+········return·head;
 ····}
 }
