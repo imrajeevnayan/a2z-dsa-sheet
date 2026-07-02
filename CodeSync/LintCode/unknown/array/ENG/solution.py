d0d4727b06b060019ce07c2aef6143abc285d511
CodeSync <codesync@users.noreply.github.com>
2026-07-02 21:24:48 +0530

    Solved: ENG (LintCode) Sheets: None

---
+++ CodeSync/LintCode/unknown/array/ENG/solution.py
@@ -0,0 +1,19 @@
+# Platform: LintCode
+# Problem: ENG
+# URL: https://www.lintcode.com/problem/366
+# Language: Python
+# Difficulty: Unknown
+# Topics: EnumerateArraySimulation, Enumerate, Array, Simulation, NetEase, Test Data Test Output
+# Runtime: N/A
+# Memory: N/A
+# Synced: 2026-07-02T15:44:33.674Z
+
+public·class·Solution·{
+····/**
+·····*·@param·n:·an·integer
+·····*·@return:·an·integer·f(n)
+·····*/
+····public·int·fibonacci(int·n)·{
+········//·write·your·code·here
+····}
+}
