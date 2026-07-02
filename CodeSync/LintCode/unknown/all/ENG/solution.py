730a3eac5067dc59467197b69361dc50b289ec92
CodeSync <codesync@users.noreply.github.com>
2026-07-02 21:25:37 +0530

    Solved: ENG (LintCode) Sheets: None

--- CodeSync/LintCode/unknown/all/ENG/solution.py
+++ CodeSync/LintCode/unknown/all/ENG/solution.py
@@ -6,13 +6,17 @@
 # Topics: EnumerateArraySimulation, Enumerate, Array, Simulation, NetEase, Test Data Test Output
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-07-02T15:45:18.788Z
+# Synced: 2026-07-02T15:46:10.437Z
 
 public·class·Solution·{
 ····public·int·fibonacci(int·n)·{
 ········if(n<=1)return·0;
-····}
 ········int·dp[]=new·int[n+1];
 ········dp[0]=0;
-········dp
+········dp[1]=1;
+········for(int·i=2;i<=n;i++){
+············dp[i]=dp[i-1]+dp[i-2];
+········}
+····}
+········dp[n];
 }
