a6d392804ec6b9f719351409f640b1539fad2a6c
CodeSync <codesync@users.noreply.github.com>
2026-07-02 21:27:14 +0530

    Solved: ENG (LintCode) Sheets: None

--- CodeSync/LintCode/unknown/array/ENG/solution.py
+++ CodeSync/LintCode/unknown/array/ENG/solution.py
@@ -3,14 +3,14 @@
 # URL: https://www.lintcode.com/problem/366
 # Language: Python
 # Difficulty: Unknown
-# Topics: EnumerateArraySimulation, Enumerate, Array, Simulation, NetEase, Test Data Commit Output
+# Topics: EnumerateArraySimulation, Enumerate, Array, Simulation, NetEase, Test Data Test Output
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-07-02T15:46:57.856Z
+# Synced: 2026-07-02T15:47:50.892Z
 
 public·class·Solution·{
 ····public·int·fibonacci(int·n)·{
-········if(n<1)return·0;
+·········if(n·<1)return·0;
 ········int·dp[]=new·int[n+1];
 ········dp[0]=0;
 ········dp[1]=1;
