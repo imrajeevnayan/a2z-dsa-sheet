3957ee29bf69adcef6650520709672a3e7bcaca3
CodeSync <codesync@users.noreply.github.com>
2026-07-02 21:23:59 +0530

    Solved: ENG (LintCode) Sheets: None

--- CodeSync/LintCode/all/ENG/solution.java
+++ CodeSync/LintCode/all/ENG/solution.java
@@ -1,26 +1,25 @@
 /*
  * Platform: LintCode
  * Problem: ENG
- * URL: https://www.lintcode.com/problem/167/
+ * URL: https://www.lintcode.com/problem/111/
  * Language: Java
  * Difficulty: Unknown
- * Topics: Linked ListSimulation, Linked List, Simulation, Test Data Test Output
+ * Topics: Dynamic Programming, DPCoordinate DP, DP, Coordinate DP, 366 Fibonacci Naive 392 House Robber Medium, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-22T17:20:09.641Z
+ * Synced: 2026-07-02T15:42:15.610Z
  */
 
-············if·(!s2.isEmpty())
-················sum·+=·s2.pop();
-
-············carry·=·sum·/·10;
-
-············ListNode·node·=·new·ListNode(sum·%·10);
-
-············node.next·=·head;
-············head·=·node;
+public·class·Solution·{
+····public·int·climbStairs(int·n)·{
+········if(n·<=2)·return·n;
+········int·a=1;
+········int·b=2;
+········for(int·i=3;i<=n;i++){
+············int·c=a+b;
+············a=b;
+············b=c;
 ········}
-
-········return·head;
+········return·b;
 ····}
 }
