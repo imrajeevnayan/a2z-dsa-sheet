3ec3c7fdfc320b4d89fcb5556454602f94fb65c1
CodeSync <codesync@users.noreply.github.com>
2026-07-02 21:24:26 +0530

    Solved: ENG (LintCode) Sheets: None

--- CodeSync/LintCode/unknown/dpcoordinate-dp/ENG/solution.java
+++ CodeSync/LintCode/unknown/dpcoordinate-dp/ENG/solution.java
@@ -7,15 +7,18 @@
  * Topics: Dynamic Programming, DPCoordinate DP, DP, Coordinate DP, 366 Fibonacci Naive 392 House Robber Medium, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-07-02T15:43:01.071Z
+ * Synced: 2026-07-02T15:43:46.442Z
  */
 
 public·class·Solution·{
 ····public·int·climbStairs(int·n)·{
 ········if(n·<=2)·return·n;
 ········int·dp[]=new·int[n+1];
-····}
-········for()
+········for(int·i=3;i<=n;i++){
 ········dp[1]=1;
-········d
+········dp[2]=2;
+············dp[i]=dp[i-1]+dp[i-2];
+········}
+····}
+········return·dp[n]
 }
