8ef8aed91f9e1318da4e65dd60f5a7472c8f366f
CodeSync <codesync@users.noreply.github.com>
2026-07-09 00:09:58 +0530

    Solved: Sort by Color (InterviewBit) Sheets: InterviewBitSet

---
+++ CodeSync/InterviewBit Sets/Sort by Color/solution.c
@@ -0,0 +1,20 @@
+/*
+ * Platform: InterviewBit
+ * Problem: Sort by Color
+ * URL: https://www.interviewbit.com/problems/sort-by-color/
+ * Language: C
+ * Difficulty: Medium
+ * Topics: Programming, Two Pointers, Description, Discussion, Submissions, Hints, 58.8%
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-07-08T18:39:56.622Z
+ */
+
+/**
+ * @input A : Integer array
+ * @input n1 : Integer array's ( A ) length
+ * 
+ * @Output Void. Just modifies the args passed by reference 
+ */
+void sortColors(int* A, int n1) {
+}
