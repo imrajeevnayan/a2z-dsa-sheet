c9053148095b5f483bc0d40eef5caaa6700c21e0
CodeSync <codesync@users.noreply.github.com>
2026-07-14 17:13:50 +0530

    Solved: 153. Find Minimum in Rotated Sorted Array (LeetCode) Sheets: Blind75, NeetCode150, TopInterview150, StriverA2Z, custom:algomaster-300, custom:neetcode-dsa-sheet, custom:shradha-khapra-dsa-sheet, custom:striver-a2z-dsa-sheet

---
+++ CodeSync/Blind 75/153. Find Minimum in Rotated Sorted Array/solution.go
@@ -0,0 +1,15 @@
+/*
+ * Platform: LeetCode
+ * Problem: 153. Find Minimum in Rotated Sorted Array
+ * URL: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/submissions/2066450688/?envType=study-plan-v2&envId=top-100-liked
+ * Language: Go
+ * Difficulty: Medium
+ * Topics: Array, Binary Search
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-07-14T11:43:39.558Z
+ */
+
+Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
