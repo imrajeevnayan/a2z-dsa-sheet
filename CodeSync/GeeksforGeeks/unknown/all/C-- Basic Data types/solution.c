b16124df2594f8deeb55ef7eed6573409ad182b9
CodeSync <codesync@users.noreply.github.com>
2026-07-15 00:37:50 +0530

    Solved: C++ Basic Data types (GeeksforGeeks) Sheets: GFG160

---
+++ CodeSync/GeeksforGeeks/unknown/all/C-- Basic Data types/solution.c
@@ -0,0 +1,19 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem: C++ Basic Data types
+ * URL: https://www.geeksforgeeks.org/problems/c-basic-data-types3128/1
+ * Language: C
+ * Difficulty: Unknown
+ * Topics: Topic Tags, Strings, CPP, Data Type, Data Structures
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-07-14T18:15:53.363Z
+ */
+
+Input:
+S=a
+output:
+1
+Explanation:
+The string clearly represents char 
+and thus the size of char is displayed.
