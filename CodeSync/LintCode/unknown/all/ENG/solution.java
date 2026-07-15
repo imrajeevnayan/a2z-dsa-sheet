c67b4cf70fd0b1dde5d30c7ef61043d5866d525d
CodeSync <codesync@users.noreply.github.com>
2026-07-15 15:13:55 +0530

    Solved: ENG (LintCode) Sheets: None

--- CodeSync/LintCode/unknown/all/ENG/solution.java
+++ CodeSync/LintCode/unknown/all/ENG/solution.java
@@ -7,19 +7,11 @@
  * Topics: HeapHash TableSort, Heap, Hash Table, Sort, Pocket Gems Amazon Bloomberg Yelp Uber, Pocket Gems, Amazon, Bloomberg
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-07-15T09:40:43.868Z
+ * Synced: 2026-07-15T09:41:29.876Z
  */
 
 public·class·Solution·{
 ····public·String[]·topKFrequentWords(String[]·words,·int·k)·{
-·····HashMap<String,Integer>map=new·HashMap<>();
-·····for(String·s·:words)map.put(s,map.getOrDefault(s,0)+1);
-·····PriorityQueue<String>·pq·=·new·PriorityQueue<>((a,·b)·->·{
-······if·(map.get(a).equals(map.get(b)))·{return·a.compareTo(b);}
-·········return·map.get(b)·-·map.get(a);··});
-·······pq.addAll(map.keySet());
-·······String·ans[]=new·String[k];
-·······for(int·i=0;i<k;i++)·ans[i]=pq.poll();
-·······return·ans;
 ····}
+······
 }
