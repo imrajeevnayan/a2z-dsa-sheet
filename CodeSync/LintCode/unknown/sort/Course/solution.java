93b9965daeab8c822cd655000492ba26acadead7
CodeSync <codesync@users.noreply.github.com>
2026-07-15 15:22:25 +0530

    Solved: Course (LintCode) Sheets: Blind75, Grind75, Grind169, NeetCode150, TopInterview150, Top100Liked, StriverA2Z, LoveBabbar450, StriverSDE, custom:Rohit-Negi_DSA_Sheet, custom:algomaster-300, custom:fraz-dsa-sheet, custom:neetcode-dsa-sheet, custom:rohit-negi_dsa_sheet, custom:striver-a2z-dsa-sheet, custom:striver-sde-sheet

---
+++ CodeSync/LintCode/unknown/sort/Course/solution.java
@@ -0,0 +1,17 @@
+/*
+ * Platform: LintCode
+ * Problem: Course
+ * URL: https://www.lintcode.com/problem/1883/description
+ * Language: Java
+ * Difficulty: Unknown
+ * Topics: Hash TableSort, Hash Table, Sort, Test Data Test Output
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-07-15T09:43:57.503Z
+ */
+
+public·class·Solution·{
+····public·List<String>·topkKeywords(int·k,·String[]·keywords,·String[]·reviews)·{
+········
+····}
+}
