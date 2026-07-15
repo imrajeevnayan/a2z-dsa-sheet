4b1d7ed3b5b15a687614cb67bd6f4c1f810f5ee4
CodeSync <codesync@users.noreply.github.com>
2026-07-15 15:19:37 +0530

    Solved: ENG (LintCode) Sheets: None

---
+++ CodeSync/LintCode/unknown/sort/ENG/solution.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: LintCode
+ * Problem: ENG
+ * URL: https://www.lintcode.com/problem/1883
+ * Language: Java
+ * Difficulty: Unknown
+ * Topics: Hash TableSort, Hash Table, Sort, Test Data Test Output
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-07-15T09:43:02.269Z
+ */
+
+public·class·Solution·{
+····/**
+·····*·@param·k:·an·integer
+·····*·@param·keywords:·a·list·of·string
+·····*·@param·reviews:·a·list·of·string
+·····*·@return:·return·the·top·k·keywords·list
+·····*/
+····public·List<String>·topkKeywords(int·k,·String[]·keywords,·String[]·reviews)·{
+········//·write·your·code·here
+····}
+}
