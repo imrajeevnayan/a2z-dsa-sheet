d0272810fbe059bc3a07a5cd8abf06ada193da72
CodeSync <codesync@users.noreply.github.com>
2026-07-15 15:19:41 +0530

    Solved: ENG (LintCode) Sheets: None

--- CodeSync/LintCode/unknown/all/ENG/solution.java
+++ CodeSync/LintCode/unknown/all/ENG/solution.java
@@ -1,17 +1,23 @@
 /*
  * Platform: LintCode
  * Problem: ENG
- * URL: https://www.lintcode.com/problem/471/
+ * URL: https://www.lintcode.com/problem/1883
  * Language: Java
  * Difficulty: Unknown
- * Topics: HeapHash TableSort, Heap, Hash Table, Sort, Pocket Gems Amazon Bloomberg Yelp Uber, Pocket Gems, Amazon, Bloomberg
+ * Topics: Hash TableSort, Hash Table, Sort, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-07-15T09:41:29.876Z
+ * Synced: 2026-07-15T09:43:02.269Z
  */
 
 public·class·Solution·{
-····public·String[]·topKFrequentWords(String[]·words,·int·k)·{
+····/**
+·····*·@param·k:·an·integer
+·····*·@param·keywords:·a·list·of·string
+·····*·@param·reviews:·a·list·of·string
+·····*·@return:·return·the·top·k·keywords·list
+·····*/
+····public·List<String>·topkKeywords(int·k,·String[]·keywords,·String[]·reviews)·{
+········//·write·your·code·here
 ····}
-······
 }
