37602d5738ad2012778e1f554a48c2ece79d4a15
CodeSync <codesync@users.noreply.github.com>
2026-07-17 14:07:37 +0530

    Solved: Majority Element (InterviewBit) Sheets: custom:Rohit-Negi_DSA_Sheet, custom:rohit-negi_dsa_sheet, StriverSDE, StriverA2Z, AlgoMaster300, custom:striver-a2z-dsa-sheet, custom:striver-sde-sheet, Grind75, Grind169, TopInterview150, Top100Liked, custom:algomaster-300, custom:arsh-goyal-dsa-sheet, custom:fraz-dsa-sheet, custom:shradha-khapra-dsa-sheet, custom:love-babbar-dsa-sheet, InterviewBitSet

--- CodeSync/AlgoMaster 300/Majority Element/solution.java
+++ CodeSync/AlgoMaster 300/Majority Element/solution.java
@@ -1,22 +1,18 @@
 /*
  * Platform: InterviewBit
  * Problem: Majority Element
- * URL: https://www.interviewbit.com/problems/majority-element/
+ * URL: https://www.interviewbit.com/problems/majority-element/submissions/
  * Language: Java
- * Difficulty: Easy
- * Topics: Programming, Greedy Algorithm, Description, Discussion, Submissions, Hints, Bulbs 23 Minutes Easy Asked in:, Turn on the Bulbs 27 Minutes Very Easy Asked in:
+ * Difficulty: Unknown
+ * Topics: Description, Discussion, Submissions, Hints
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-07-17T08:33:55.961Z
+ * Synced: 2026-07-17T08:34:42.671Z
  */
 
 public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
     public int majorityElement(final List<Integer> A) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        for(int n:A)map.put(n,map.getOrDefault(n,0)+1);
-        for(int n:map.keySet()){
-            if(map.get(n)> A.size()/2) return n;
-        }
-        return -1;
-    }
 }
+
+    }
