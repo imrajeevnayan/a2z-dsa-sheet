12922e5469984441221679c2d66d1c1ee9c9cd33
CodeSync <codesync@users.noreply.github.com>
2026-07-18 23:37:26 +0530

    Solved: All Subsequences of a String (GeeksforGeeks) Sheets: custom:love-babbar-dsa-sheet, custom:shradha-khapra-dsa-sheet, GFG160

---
+++ CodeSync/GeeksforGeeks/all/All Subsequences of a String/solution.c
@@ -0,0 +1,16 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem: All Subsequences of a String
+ * URL: https://www.geeksforgeeks.org/problems/power-set4302/1
+ * Language: C
+ * Difficulty: Unknown
+ * Topics: Company Tags Snapdeal, Company Tags, Snapdeal, Topic Tags, Mathematical, Bit Magic, Related Articles, Power Set
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-07-18T09:02:55.354Z
+ */
+
+Input : s = "abc"
+Output: ["","a", "ab", "abc", "ac", "b", "bc", "c"]
+Explanation: There are a total of 8 non-empty subsequences for the given string. 
+These subsequences are listed above in lexicographical order.
