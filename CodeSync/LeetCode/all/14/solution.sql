1e133bc16de6449de078ef3836f574e6d7bcc0eb
CodeSync <codesync@users.noreply.github.com>
2026-08-07 12:49:42 +0530

    Solved: 14 (LeetCode) Sheets: None

---
+++ CodeSync/LeetCode/all/14/solution.sql
@@ -0,0 +1,11 @@
+-- Platform: LeetCode
+-- Problem: 14
+-- URL: https://leetcode.com/problems/find-customer-referee/description/?envType=problem-list-v2&envId=dmjree02
+-- Language: SQL
+-- Difficulty: Easy
+-- Topics: Database
+-- Runtime: N/A
+-- Memory: N/A
+-- Synced: 2026-08-07T07:19:32.433Z
+
+# Write your MySQL query statement below
