a0c497b8035f10a561be5a692e396ff96967d579
CodeSync <codesync@users.noreply.github.com>
2026-08-07 10:28:07 +0530

    Solved: - (LeetCode) Sheets: None

---
+++ CodeSync/LeetCode/easy/string/-/solution.c
@@ -0,0 +1,18 @@
+/*
+ * Platform: LeetCode
+ * Problem: -
+ * URL: https://leetcode.com/problems/count-asterisks/description/?envType=problem-list-v2&envId=string
+ * Language: C
+ * Difficulty: Easy
+ * Topics: String
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-08-06T20:25:47.772Z
+ */
+
+Input: s = "l|*e*et|c**o|*de|"
+Output: 2
+Explanation: The considered characters are underlined: "l|*e*et|c**o|*de|".
+The characters between the first and second '|' are excluded from the answer.
+Also, the characters between the third and fourth '|' are excluded from the answer.
+There are 2 asterisks considered. Therefore, we return 2.
