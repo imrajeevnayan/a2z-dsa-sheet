3685591ebdc48101d217a45fc37073f788c45954
CodeSync <codesync@users.noreply.github.com>
2026-08-07 12:50:28 +0530

    Solved: Find Customer Referee (LeetCode) Sheets: None

---
+++ CodeSync/LeetCode/easy/all/Find Customer Referee/solution.sql
@@ -0,0 +1,11 @@
+-- Platform: LeetCode
+-- Problem: Find Customer Referee
+-- URL: https://leetcode.com/problems/find-customer-referee/description/?envType=problem-list-v2&envId=dmjree02
+-- Language: SQL
+-- Difficulty: Easy
+-- Topics: Database
+-- Runtime: N/A
+-- Memory: N/A
+-- Synced: 2026-08-07T07:20:23.522Z
+
+# Write your MySQL query statement below
