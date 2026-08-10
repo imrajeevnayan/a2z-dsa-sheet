3cc55212bcc80b55265bc87adea9c7af120d543b
CodeSync <codesync@users.noreply.github.com>
2026-08-10 22:58:11 +0530

    Solved: 79. Word Search (LeetCode) Sheets: custom:arsh-goyal-dsa-sheet, custom:fraz-dsa-sheet, custom:neetcode-dsa-sheet, custom:shradha-khapra-dsa-sheet, custom:striver-a2z-dsa-sheet, custom:Rohit-Negi_DSA_Sheet, custom:rohit-negi_dsa_sheet, custom:algomaster-300

---
+++ CodeSync/LeetCode/medium/backtracking/79. Word Search/solution.c
@@ -0,0 +1,14 @@
+/*
+ * Platform: LeetCode
+ * Problem: 79. Word Search
+ * URL: https://leetcode.com/problems/word-search/description/?envType=problem-list-v2&envId=dxlqos71
+ * Language: C
+ * Difficulty: Medium
+ * Topics: Array, String, Backtracking, Depth-First Search, Matrix
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-08-10T17:27:27.051Z
+ */
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
