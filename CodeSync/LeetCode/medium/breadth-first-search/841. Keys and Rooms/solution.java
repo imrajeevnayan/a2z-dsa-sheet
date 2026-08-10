b2543f5f02c57289192934f75f9afa7f0a91bd8f
CodeSync <codesync@users.noreply.github.com>
2026-08-10 22:56:49 +0530

    Solved: 841. Keys and Rooms (LeetCode) Sheets: custom:fraz-dsa-sheet

---
+++ CodeSync/LeetCode/medium/breadth-first-search/841. Keys and Rooms/solution.java
@@ -0,0 +1,20 @@
+/*
+ * Platform: LeetCode
+ * Problem: 841. Keys and Rooms
+ * URL: https://leetcode.com/problems/keys-and-rooms/description/?envType=problem-list-v2&envId=daeijq25
+ * Language: Java
+ * Difficulty: Medium
+ * Topics: Depth-First Search, Breadth-First Search, Graph Theory
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-08-10T17:26:38.853Z
+ */
+
+Input: rooms = [[1],[2],[3],[]]
+Output: true
+Explanation: 
+We visit room 0 and pick up key 1.
+We then visit room 1 and pick up key 2.
+We then visit room 2 and pick up key 3.
+We then visit room 3.
+Since we were able to visit every room, we return true.
