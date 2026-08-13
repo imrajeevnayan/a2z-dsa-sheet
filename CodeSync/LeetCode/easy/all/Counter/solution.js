e9c1d78a62332153736c12f7ef113c1950eae452
CodeSync <codesync@users.noreply.github.com>
2026-08-13 22:36:48 +0530

    Solved: Counter (LeetCode) Sheets: None

---
+++ CodeSync/LeetCode/easy/all/Counter/solution.js
@@ -0,0 +1,29 @@
+/*
+ * Platform: LeetCode
+ * Problem: Counter
+ * URL: https://leetcode.com/problems/counter/description/
+ * Language: JavaScript
+ * Difficulty: Easy
+ * Topics: Uncategorized
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-08-12T12:16:37.922Z
+ */
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
