06733592f69f6848a95333755404ef498174e8c5
CodeSync <codesync@users.noreply.github.com>
2026-08-14 22:17:21 +0530

    Solved: 21 (LeetCode) Sheets: custom:arsh-goyal-dsa-sheet, custom:fraz-dsa-sheet, custom:striver-a2z-dsa-sheet, custom:algomaster-300

---
+++ CodeSync/LeetCode/hard/array/21/solution.java
@@ -0,0 +1,17 @@
+/*
+ * Platform: LeetCode
+ * Problem: 21
+ * URL: https://leetcode.com/problems/split-array-largest-sum/description/?envType=problem-list-v2&envId=prefix-sum
+ * Language: Java
+ * Difficulty: Hard
+ * Topics: Array, Binary Search, Dynamic Programming, Greedy, Prefix Sum
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-08-14T16:46:25.233Z
+ */
+
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        
+    }
+}
