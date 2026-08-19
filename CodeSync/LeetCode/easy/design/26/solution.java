58c9f25905bedce1739e40f83058b047d537d3df
CodeSync <codesync@users.noreply.github.com>
2026-08-19 19:25:40 +0530

    Solved: 26 (LeetCode) Sheets: custom:arsh-goyal-dsa-sheet, custom:fraz-dsa-sheet, custom:shradha-khapra-dsa-sheet, custom:striver-a2z-dsa-sheet, custom:striver-sde-sheet, custom:love-babbar-dsa-sheet

---
+++ CodeSync/LeetCode/easy/design/26/solution.java
@@ -0,0 +1,27 @@
+/*
+ * Platform: LeetCode
+ * Problem: 26
+ * URL: https://leetcode.com/problems/implement-stack-using-queues/description/?envType=problem-list-v2&envId=stack
+ * Language: Java
+ * Difficulty: Easy
+ * Topics: Stack, Design, Queue
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-08-19T13:54:30.078Z
+ */
+
+class MyStack {
+
+    public MyStack() {
+        
+    }
+    
+    public void push(int x) {
+        
+    }
+    
+    public int pop() {
+        
+    }
+    
+    public int top() {
