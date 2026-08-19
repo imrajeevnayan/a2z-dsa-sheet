c8f8440c70f08fbdddde5119fa939bb2b2c8b1ac
CodeSync <codesync@users.noreply.github.com>
2026-08-19 19:25:18 +0530

    Solved: 26 (LeetCode) Sheets: custom:arsh-goyal-dsa-sheet, custom:fraz-dsa-sheet, custom:shradha-khapra-dsa-sheet, custom:striver-a2z-dsa-sheet, custom:striver-sde-sheet, custom:love-babbar-dsa-sheet

--- CodeSync/shradha-khapra-dsa-sheet/26/solution.java
+++ CodeSync/shradha-khapra-dsa-sheet/26/solution.java
@@ -1,26 +1,27 @@
 /*
  * Platform: LeetCode
  * Problem: 26
- * URL: https://leetcode.com/problems/remove-outermost-parentheses/description/?envType=problem-list-v2&envId=stack
+ * URL: https://leetcode.com/problems/implement-stack-using-queues/description/?envType=problem-list-v2&envId=stack
  * Language: Java
  * Difficulty: Easy
- * Topics: String, Stack, Bracket Sequences
+ * Topics: Stack, Design, Queue
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-08-19T13:51:35.728Z
+ * Synced: 2026-08-19T13:54:30.078Z
  */
 
-class Solution {
-    public String removeOuterParentheses(String s) {
-        StringBuilder sb = new StringBuilder();
-        int count = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                if (count++ > 0) sb.append(c);
-            } else {
-                if (--count > 0) sb.append(c);
-            }
-        }
-        return sb.toString();
+class MyStack {
+
+    public MyStack() {
+        
     }
-}
+    
+    public void push(int x) {
+        
+    }
+    
+    public int pop() {
+        
+    }
+    
+    public int top() {
