047bf5fc8e27e5fff8089044e936896ef5f2c1bb
CodeSync <codesync@users.noreply.github.com>
2026-09-06 01:17:53 +0530

    Solved: 0 (LeetCode) Sheets: custom:Rohit-Negi_DSA_Sheet, custom:rohit-negi_dsa_sheet, custom:arsh-goyal-dsa-sheet, custom:love-babbar-dsa-sheet, custom:shradha-khapra-dsa-sheet

--- CodeSync/LeetCode/all/0/solution.java
+++ CodeSync/LeetCode/all/0/solution.java
@@ -1,13 +1,36 @@
 /*
  * Platform: LeetCode
  * Problem: 0
- * URL: https://leetcode.com/problems/valid-palindrome/submissions/2130673263/
+ * URL: https://leetcode.com/submissions/detail/2131938642/
  * Language: Java
  * Difficulty: Unknown
  * Topics: Uncategorized
- * Runtime: N/A
- * Memory: N/A
- * Synced: 2026-09-04T15:51:26.008Z
+ * Runtime: 0 ms
+ * Memory: 44.08 MB
+ * Synced: 2026-09-05T17:01:56.502Z
  */
 
-[ Time taken: 24s ]
+1class Solution {
+2    public ListNode rotateRight(ListNode head, int k) {
+3        if(head==null || head.next==null ||k==0) return head;
+4
+5        int n=1;
+6        ListNode tail=head;
+7        while(tail.next!=null){
+8            tail=tail.next;
+9            n++;
+10        }
+11        k=k%n;
+12        if(k==0)return head;
+13        // circular banooo
+14        tail.next=head;
+15        int steps=n-k;
+16        ListNode newTail=head;
+17        for(int i=1;i<steps;i++) newTail=newTail.next;
+18        ListNode newHead=newTail.next;
+19        newTail.next=null;
+20        
+21        return newHead;
+22            
+23    }
+24}
