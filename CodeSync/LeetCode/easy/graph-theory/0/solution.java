383c15815612280d3097fb332255a08d4b614b75
CodeSync <codesync@users.noreply.github.com>
2026-09-10 23:53:34 +0530

    Solved: 0 (LeetCode) Sheets: custom:Rohit-Negi_DSA_Sheet, custom:rohit-negi_dsa_sheet, custom:arsh-goyal-dsa-sheet, custom:love-babbar-dsa-sheet, custom:shradha-khapra-dsa-sheet

---
+++ CodeSync/LeetCode/easy/graph-theory/0/solution.java
@@ -0,0 +1,17 @@
+/*
+ * Platform: LeetCode
+ * Problem: 0
+ * URL: https://leetcode.com/problems/find-center-of-star-graph/description/?envType=problem-list-v2&envId=graph
+ * Language: Java
+ * Difficulty: Easy
+ * Topics: Graph Theory
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-09-09T13:22:08.019Z
+ */
+
+class Solution {
+    public int findCenter(int[][] edges) {
+        
+    }
+}
