e4e5671ae5686bfcde2975c139a6b86494c690f1
CodeSync <codesync@users.noreply.github.com>
2026-09-12 22:46:30 +0530

    Solved: Department Highest Salary (MCQ Problem) (Coding Ninjas (Code360)) Sheets: None

--- CodeSync/Coding Ninjas -Code360-/all/Department Highest Salary -MCQ Problem-/solution.sql
+++ CodeSync/Coding Ninjas -Code360-/all/Department Highest Salary -MCQ Problem-/solution.sql
@@ -3,23 +3,29 @@
 -- URL: https://www.naukri.com/code360/problems/department-highest-salary_2119329
 -- Language: SQL
 -- Difficulty: Unknown
--- Topics: Uncategorized
+-- Topics: SQL, 40 Easy Students DB 0, 40 Easy IMDb Genre 0, 120 Hard Sales Executive 0, 40 Easy IMDb Max Weighted Rating 0, 40 Easy Big Countries 0, 40 Easy Director's Actor 0, 40 Easy Rank Scores 0
 -- Runtime: N/A
 -- Memory: N/A
--- Synced: 2026-09-12T17:15:24.279Z
+-- Synced: 2026-09-12T17:16:11.731Z
 
 WITH CTE AS (
- 
-SELECT  B.NAME, MAX(SALARY) AS SALARY
- 
-FROM  DEPARTMENT B JOIN EMPLOYEE A
- 
-ON A.DEPARTMENTID = B.ID
- 
-GROUP BY B.NAME
- 
+    SELECT
+        B.NAME AS Department,
+        A.NAME AS Employee,
+        A.SALARY,
+        DENSE_RANK() OVER (
+            PARTITION BY A.DEPARTMENTID
+            ORDER BY A.SALARY DESC
+        ) AS rnk
+    FROM DEPARTMENT B
+    JOIN EMPLOYEE A
+        ON A.DEPARTMENTID = B.ID
 )
  
-SELECT CTE.NAME AS "Department",N.NAME AS "Employee",CTE.SALARY AS "salary"  FROM CTE INNER JOIN EMPLOYEE N
- 
-ON CTE.SALARY = N.SALARY ORDER BY CTE.NAME,N.NAME desc
+SELECT
+    Department,
+    Employee,
+    Salary
+FROM CTE
+WHERE rnk = 1
+ORDER BY Department, Employee DESC;
