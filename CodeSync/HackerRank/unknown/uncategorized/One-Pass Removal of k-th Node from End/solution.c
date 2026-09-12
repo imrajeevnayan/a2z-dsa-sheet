49dd02ce43b7af8c718d8eeb02e75f1e4a04a5c4
CodeSync <codesync@users.noreply.github.com>
2026-09-12 12:05:58 +0530

    Solved: One-Pass Removal of k-th Node from End (HackerRank) Sheets: None

---
+++ CodeSync/HackerRank/unknown/uncategorized/One-Pass Removal of k-th Node from End/solution.c
@@ -0,0 +1,38 @@
+/*
+ * Platform: HackerRank
+ * Problem: One-Pass Removal of k-th Node from End
+ * URL: https://www.hackerrank.com/contests/software-engineer-prep-kit/challenges/one-pass-removal-kth-from-end/problem
+ * Language: C
+ * Difficulty: Unknown
+ * Topics: Uncategorized
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-09-11T15:11:12.316Z
+ */
+
+#include <assert.h>
+
+/*
+ * Complete the 'removeKthNodeFromEnd' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST head
+ *  2. INTEGER k
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+
+SinglyLinkedListNode* removeKthNodeFromEnd(SinglyLinkedListNode* head, int k) {
+
+}
+
+int main()
