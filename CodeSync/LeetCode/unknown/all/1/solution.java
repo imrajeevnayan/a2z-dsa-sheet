7718e1d0f4e8624bba05491e6910d38f00311c80
CodeSync <codesync@users.noreply.github.com>
2026-09-14 18:35:28 +0530

    Solved: 1 (LeetCode) Sheets: custom:Rohit-Negi_DSA_Sheet, custom:rohit-negi_dsa_sheet, custom:algomaster-300, custom:neetcode-dsa-sheet, custom:love-babbar-dsa-sheet, custom:shradha-khapra-dsa-sheet

--- CodeSync/LeetCode/unknown/all/1/solution.java
+++ CodeSync/LeetCode/unknown/all/1/solution.java
@@ -1,25 +1,25 @@
 /*
  * Platform: LeetCode
  * Problem: 1
- * URL: https://leetcode.com/discuss/post/8433297/fixed-size-sliding-window-pattern-comple-srmf/
+ * URL: https://leetcode.com/submissions/detail/2141174492/
  * Language: Java
  * Difficulty: Unknown
  * Topics: Uncategorized
- * Runtime: N/A
- * Memory: N/A
- * Synced: 2026-09-13T20:49:29.981Z
+ * Runtime: 24 ms
+ * Memory: 49.14 MB
+ * Synced: 2026-09-14T12:29:45.369Z
  */
 
-Subarray / Substring?
-
-        ↓
-
-Exactly K Elements?
-
-        ↓
-
-Need Every Window?
-
-        ↓
-
-Maximum / Minimum / Count?
+1class Solution {
+2      public int subarraySum(int[] arr, int k) {
+3        Map<Integer, Integer> map = new HashMap<>();
+4          map.put(0, 1);
+5         int sum = 0, count = 0;
+6          for (int n : arr) {
+7                 sum += n;
+8            if(map.containsKey(sum-k)) count+=map.get(sum-k);
+9            map.put(sum,map.getOrDefault(sum,0)+1); 
+10         }
+11         return count;
+12    }
+13}
