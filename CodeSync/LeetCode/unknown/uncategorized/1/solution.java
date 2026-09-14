3ab39acf46140eba15a3bade93405adb325e2547
CodeSync <codesync@users.noreply.github.com>
2026-09-14 14:38:44 +0530

    Solved: 1 (LeetCode) Sheets: custom:Rohit-Negi_DSA_Sheet, custom:rohit-negi_dsa_sheet, custom:algomaster-300, custom:neetcode-dsa-sheet, custom:love-babbar-dsa-sheet, custom:shradha-khapra-dsa-sheet

--- CodeSync/LeetCode/unknown/uncategorized/1/solution.java
+++ CodeSync/LeetCode/unknown/uncategorized/1/solution.java
@@ -1,25 +1,24 @@
 /*
  * Platform: LeetCode
  * Problem: 1
- * URL: https://leetcode.com/discuss/post/8433297/fixed-size-sliding-window-pattern-comple-srmf/
+ * URL: https://leetcode.com/submissions/detail/2141038812/
  * Language: Java
  * Difficulty: Unknown
  * Topics: Uncategorized
- * Runtime: N/A
- * Memory: N/A
- * Synced: 2026-09-13T20:49:29.981Z
+ * Runtime: 3 ms
+ * Memory: 44.52 MB
+ * Synced: 2026-09-13T22:31:09.353Z
  */
 
-Subarray / Substring?
-
-        ↓
-
-Exactly K Elements?
-
-        ↓
-
-Need Every Window?
-
-        ↓
-
-Maximum / Minimum / Count?
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3        if(s.length()!=t.length())return false;
+4        int freq[]=new int[26];
+5        for(char c:s.toCharArray())freq[c-'a']++;
+6        for(char c:t.toCharArray())freq[c-'a']--;
+7        for(int f:freq){
+8            if(f!=0)return false;
+9        }
+10        return true;
+11    }
+12}
