2d48147e5ac5987fc793b831680eca8a806712e7
CodeSync <codesync@users.noreply.github.com>
2026-09-17 10:13:08 +0530

    Solved: 0 (LeetCode) Sheets: Blind75, NeetCode150, TopInterview150, Top100Liked, StriverA2Z, custom:algomaster-300, custom:arsh-goyal-dsa-sheet, custom:fraz-dsa-sheet, custom:neetcode-dsa-sheet, custom:shradha-khapra-dsa-sheet, custom:striver-a2z-dsa-sheet, custom:striver-sde-sheet, custom:Rohit-Negi_DSA_Sheet, custom:rohit-negi_dsa_sheet, custom:love-babbar-dsa-sheet

--- CodeSync/rohit-negi dsa sheet/0/solution.java
+++ CodeSync/rohit-negi dsa sheet/0/solution.java
@@ -1,40 +1,14 @@
 /*
  * Platform: LeetCode
  * Problem: 0
- * URL: https://leetcode.com/submissions/detail/2144104522/
+ * URL: https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
  * Language: Java
- * Difficulty: Unknown
- * Topics: Uncategorized
- * Runtime: 1 ms
- * Memory: 46.53 MB
- * Synced: 2026-09-16T20:37:31.581Z
+ * Difficulty: Medium
+ * Topics: Tree, Depth-First Search, Binary Search Tree, Binary Tree
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-09-17T03:59:46.836Z
  */
 
-1class Solution {
-2    public int kthSmallest(TreeNode root, int k) {
-3
-4        Stack<TreeNode> stack = new Stack<>();
-5        TreeNode curr = root;
-6
-7        while (true) {
-8
-9            // Left side ke nodes stack mein daalo
-10            while (curr != null) {
-11                stack.push(curr);
-12                curr = curr.left;
-13            }
-14
-15            // Smallest remaining node
-16            curr = stack.pop();
-17            k--;
-18
-19            // kth smallest mil gaya
-20            if (k == 0) {
-21                return curr.val;
-22            }
-23
-24            // Ab right subtree explore karo
-25            curr = curr.right;
-26        }
-27    }
-28}
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
