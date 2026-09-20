24e8f612ca80a2cd30eeb7f87359d3f44cbb92ad
CodeSync <codesync@users.noreply.github.com>
2026-09-20 16:06:57 +0530

    Solved: 2 Sum (InterviewBit) Sheets: Blind75, Grind75, Grind169, NeetCode150, LeetCode75, TopInterview150, Top100Liked, StriverA2Z, LoveBabbar450, NishantChahar151, KushalVijay20, ApnaCollege375, FrazBhaiya, AlgoMaster75, SixComp30Days, Striver79, AtharvaPatil150, StriverSDE, custom:algomaster-300, custom:arsh-goyal-dsa-sheet, custom:fraz-dsa-sheet, custom:neetcode-dsa-sheet, custom:striver-a2z-dsa-sheet, custom:striver-sde-sheet, custom:Rohit-Negi_DSA_Sheet, custom:rohit-negi_dsa_sheet, custom:shradha-khapra-dsa-sheet, InterviewBitSet, custom:love-babbar-dsa-sheet

---
+++ CodeSync/Atharva Patil-s 150 Sheet/2 Sum/solution.java
@@ -0,0 +1,16 @@
+/*
+ * Platform: InterviewBit
+ * Problem: 2 Sum
+ * URL: https://www.interviewbit.com/problems/2-sum/
+ * Language: Java
+ * Difficulty: Easy
+ * Topics: Programming, Hashing, Description, Discussion, Submissions, Hints, An Increment Problem 51 Minutes Medium Asked in:, Check Palindrome! 19 Minutes Easy Asked in:
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-09-20T10:28:08.492Z
+ */
+
+public class Solution {
+    public ArrayList<Integer> twoSum(final List<Integer> A, int B) {
+    }
+}
