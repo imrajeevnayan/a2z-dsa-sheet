c0a7feb4496b19c82e333280776f50180005029e
CodeSync <codesync@users.noreply.github.com>
2026-09-25 02:08:13 +0530

    Solved: Detect a loop in LL (TakeUForward) Sheets: None

--- CodeSync/TakeUForward/medium/all/Detect a loop in LL/solution.java
+++ CodeSync/TakeUForward/medium/all/Detect a loop in LL/solution.java
@@ -7,17 +7,17 @@
  * Topics: Prep, Explore, My Spaces, Dashboard, Prep Hub, DSA, SQL, Planly
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-09-24T20:02:05.172Z
+ * Synced: 2026-09-24T20:02:07.814Z
  */
 
 // Slow 1 step
         ListNode fast = head;
 
         while (fast != null && fast.next != null) {
-        ListNode slow = head;
 
         // Fast pointer 2 steps chalega
     public boolean hasCycle(ListNode head) {
 
-        // Slow pointer 1 step chalega
+        ListNode slow = head;
 class Solution {
+            slow = slow.next;
