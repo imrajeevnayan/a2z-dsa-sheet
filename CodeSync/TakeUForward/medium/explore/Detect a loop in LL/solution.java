8d0dc679340bceca21ea576b7233832ace870f77
CodeSync <codesync@users.noreply.github.com>
2026-09-25 02:04:51 +0530

    Solved: Detect a loop in LL (TakeUForward) Sheets: None

---
+++ CodeSync/TakeUForward/medium/explore/Detect a loop in LL/solution.java
@@ -0,0 +1,13 @@
+/*
+ * Platform: TakeUForward
+ * Problem: Detect a loop in LL
+ * URL: https://takeuforward.org/practice/dsa/detect-a-loop-in-ll
+ * Language: Java
+ * Difficulty: Medium
+ * Topics: Prep, Explore, My Spaces, Dashboard, Prep Hub, DSA, SQL, Planly
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-09-24T20:01:10.673Z
+ */
+
+/*Definition of singly linked list:
