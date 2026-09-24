9f2c8ad20a57d8c7ec9553bd47c87928f22f802a
CodeSync <codesync@users.noreply.github.com>
2026-09-25 02:14:01 +0530

    Solved: Detect a loop in LL (TakeUForward) Sheets: None

--- CodeSync/TakeUForward/medium/explore/Detect a loop in LL/solution.java
+++ CodeSync/TakeUForward/medium/explore/Detect a loop in LL/solution.java
@@ -7,14 +7,10 @@
  * Topics: Prep, Explore, My Spaces, Dashboard, Prep Hub, DSA, SQL, Planly
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-09-24T20:02:26.182Z
+ * Synced: 2026-09-24T20:02:28.201Z
  */
 
 // Slow 1 step
-        while (fast != null && fast.next != null) {
-    public boolean hasCycle(ListNode head) {
-        ListNode slow = head,fast = head;
-class Solution {
             slow = slow.next;
             // Fast 2 steps
             fast = fast.next.next;
@@ -22,3 +18,8 @@ class Solution {
             // Agar dono mil gaye to cycle hai
             if (slow == fast) {
                 return true;
+            }
+        }
+
+        // Fast null tak pahunch gaya
+        // Matlab cycle nahi hai
