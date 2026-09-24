7e27960850cdc531ef47ae364d03dc892c555d67
CodeSync <codesync@users.noreply.github.com>
2026-09-25 02:05:21 +0530

    Solved: Detect a loop in LL (TakeUForward) Sheets: None

--- CodeSync/TakeUForward/medium/explore/Detect a loop in LL/solution.java
+++ CodeSync/TakeUForward/medium/explore/Detect a loop in LL/solution.java
@@ -7,7 +7,18 @@
  * Topics: Prep, Explore, My Spaces, Dashboard, Prep Hub, DSA, SQL, Planly
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-09-24T20:01:10.673Z
+ * Synced: 2026-09-24T20:01:13.970Z
  */
 
 /*Definition of singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
