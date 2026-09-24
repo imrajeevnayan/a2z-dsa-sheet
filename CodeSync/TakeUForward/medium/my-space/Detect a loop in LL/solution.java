b39c5592747c5c17579016939022d21e3b051c29
CodeSync <codesync@users.noreply.github.com>
2026-09-25 02:13:39 +0530

    Solved: Detect a loop in LL (TakeUForward) Sheets: None

--- CodeSync/TakeUForward/medium/my-space/Detect a loop in LL/solution.java
+++ CodeSync/TakeUForward/medium/my-space/Detect a loop in LL/solution.java
@@ -7,7 +7,7 @@
  * Topics: Prep, Explore, My Spaces, Dashboard, Prep Hub, DSA, SQL, Planly
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-09-24T20:02:24.948Z
+ * Synced: 2026-09-24T20:02:26.182Z
  */
 
 // Slow 1 step
@@ -16,9 +16,9 @@
         ListNode slow = head,fast = head;
 class Solution {
             slow = slow.next;
-
             // Fast 2 steps
             fast = fast.next.next;
 
             // Agar dono mil gaye to cycle hai
             if (slow == fast) {
+                return true;
