101eeea78efefede580452e886c5e2314fcd7148
CodeSync <codesync@users.noreply.github.com>
2026-09-25 01:33:30 +0530

    Solved: Two Sum (TakeUForward) Sheets: Blind75, Grind75, Grind169, NeetCode150, LeetCode75, TopInterview150, Top100Liked, StriverA2Z, LoveBabbar450, NishantChahar151, KushalVijay20, ApnaCollege375, FrazBhaiya, AlgoMaster75, SixComp30Days, Striver79, AtharvaPatil150, StriverSDE, custom:algomaster-300, custom:arsh-goyal-dsa-sheet, custom:fraz-dsa-sheet, custom:neetcode-dsa-sheet, custom:striver-a2z-dsa-sheet, custom:striver-sde-sheet, custom:shradha-khapra-dsa-sheet, custom:love-babbar-dsa-sheet

---
+++ CodeSync/fraz-dsa-sheet/Two Sum/solution.java
@@ -0,0 +1,27 @@
+/*
+ * Platform: TakeUForward
+ * Problem: Two Sum
+ * URL: https://takeuforward.org/practice/dsa/two-sum
+ * Language: Java
+ * Difficulty: Medium
+ * Topics: Prep, Explore, My Spaces, Dashboard, Prep Hub, DSA, SQL, Planly
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-09-24T19:43:39.514Z
+ */
+
+class Solution {
+    public int[] nextLargerElement(int[] arr) {
+        int n=arr.length;
+        int [] ans=new int[n];
+        Arrays.fill(ans,-1);
+        Deque<Integer>st=new ArrayDeque<>();
+        for(int i=0;i<arr.length;i++){
+           while(!st.isEmpty() && arr[i]>arr[st.peek()]){
+            ans[st.pop()]=arr[i];
+           }
+           st.push(i);
+        }
+      return ans;
+    }
+}
