ffe9c783782ab2b32dd2b107dd19a9662dbdc522
CodeSync <codesync@users.noreply.github.com>
2026-09-25 01:38:17 +0530

    Solved: Reverse a LL (TakeUForward) Sheets: None

---
+++ CodeSync/TakeUForward/all/Reverse a LL/solution.java
@@ -0,0 +1,27 @@
+/*
+ * Platform: TakeUForward
+ * Problem: Reverse a LL
+ * URL: https://takeuforward.org/practice/dsa/reverse-a-ll
+ * Language: Java
+ * Difficulty: Medium
+ * Topics: Prep, Explore, My Spaces, Dashboard, Prep Hub, DSA, SQL, Planly
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-09-24T19:44:48.514Z
+ */
+
+class Solution {
+    public int[] nextLargerElement(int[] arr) {
+        int n=arr.length;
+        int [] ans=new int[n];
+        Arrays.fill(ans,-1);
+        Deque<Integer>st=new ArrayDeque<>();
+        for(int i=0;i<arr.length;i++){
+           while(!st.isEmpty() && arr[i]>arr[st.peek()]){
+            ans[st.pop()]=arr[i];
+           }
+           st.push(i);
+        }
+      return ans;
+    }
+}
