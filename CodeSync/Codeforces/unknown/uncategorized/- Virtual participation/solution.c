ae8a24432792898719cf533a1d232f90adba386c
CodeSync <codesync@users.noreply.github.com>
2026-09-24 12:03:11 +0530

    Solved: → Virtual participation (Codeforces) Sheets: None

---
+++ CodeSync/Codeforces/unknown/uncategorized/- Virtual participation/solution.c
@@ -0,0 +1,13 @@
+/*
+ * Platform: Codeforces
+ * Problem: → Virtual participation
+ * URL: https://codeforces.com/contest/2249
+ * Language: C
+ * Difficulty: Unknown
+ * Topics: Uncategorized
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-09-24T04:11:48.005Z
+ */
+
+Due to a possible display bug, if you see a requirement in D1D/D2F to output the number of valid matrices, ignore it and refresh the statement. You only need to output a valid construction.
