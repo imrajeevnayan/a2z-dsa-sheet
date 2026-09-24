0247dcb2e5d02055f37a4e82b7b641230102cb31
CodeSync <codesync@users.noreply.github.com>
2026-09-25 01:14:56 +0530

    Solved: 3 Sum (TakeUForward) Sheets: Blind75, Grind75, Grind169, NeetCode150, LeetCode75, TopInterview150, Top100Liked, StriverA2Z, LoveBabbar450, NishantChahar151, KushalVijay20, ApnaCollege375, FrazBhaiya, AlgoMaster75, SixComp30Days, Striver79, AtharvaPatil150, StriverSDE, custom:algomaster-300, custom:arsh-goyal-dsa-sheet, custom:fraz-dsa-sheet, custom:neetcode-dsa-sheet, custom:striver-a2z-dsa-sheet, custom:striver-sde-sheet, custom:Rohit-Negi_DSA_Sheet, custom:rohit-negi_dsa_sheet, custom:shradha-khapra-dsa-sheet, InterviewBitSet, custom:love-babbar-dsa-sheet

---
+++ CodeSync/fraz-dsa-sheet/3 Sum/solution.java
@@ -0,0 +1,24 @@
+/*
+ * Platform: TakeUForward
+ * Problem: 3 Sum
+ * URL: https://takeuforward.org/practice/dsa/3-sum
+ * Language: Java
+ * Difficulty: Medium
+ * Topics: Prep, Explore, My Spaces, Dashboard, Prep Hub, DSA, SQL, Planly
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-09-24T19:20:07.372Z
+ */
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        Set<List<Integer>> set = new HashSet<>();
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            Set<Integer> seen = new HashSet<>();
+
+            for (int j = i + 1; j < nums.length; j++) {
