adfcc2ebaffab78e37420ab79747788f9ab43cc1
CodeSync <codesync@users.noreply.github.com>
2026-09-25 14:38:01 +0530

    Solved: Diameter of Binary Tree (TakeUForward) Sheets: Grind75, Grind169, NeetCode150, Top100Liked, StriverA2Z, custom:algomaster-300, custom:arsh-goyal-dsa-sheet, custom:fraz-dsa-sheet, custom:neetcode-dsa-sheet, custom:striver-a2z-dsa-sheet, custom:striver-sde-sheet, custom:Rohit-Negi_DSA_Sheet, custom:love-babbar-dsa-sheet, custom:rohit-negi_dsa_sheet, custom:shradha-khapra-dsa-sheet

--- CodeSync/Grind 75/Diameter of Binary Tree/solution.java
+++ CodeSync/Grind 75/Diameter of Binary Tree/solution.java
@@ -7,7 +7,7 @@
  * Topics: Prep, Explore, My Spaces, Dashboard, Prep Hub, DSA, SQL, Planly
  * Runtime: 0.794 ms
  * Memory: N/A
- * Synced: 2026-09-25T06:12:13.926Z
+ * Synced: 2026-09-25T06:13:13.928Z
  */
 
 class Solution {
