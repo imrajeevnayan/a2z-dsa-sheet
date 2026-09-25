00edf3e594f846e8f236706b190bcf65ef27e9b5
CodeSync <codesync@users.noreply.github.com>
2026-09-25 15:59:42 +0530

    Solved: Remove Consecutive Characters (InterviewBit) Sheets: custom:Rohit-Negi_DSA_Sheet, custom:rohit-negi_dsa_sheet, InterviewBitSet

---
+++ CodeSync/rohit-negi dsa sheet/Remove Consecutive Characters/solution.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: InterviewBit
+ * Problem: Remove Consecutive Characters
+ * URL: https://www.interviewbit.com/problems/remove-consecutive-characters/
+ * Language: Java
+ * Difficulty: Easy
+ * Topics: Programming, Strings, Description, Discussion, Submissions, Hints, Amazing Subarrays 26 Minutes Easy Asked in:, Convert to Palindrome 40 Minutes Easy Asked in:
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-09-25T10:01:40.298Z
+ */
+
+A = "aabcd"
+B = 2
