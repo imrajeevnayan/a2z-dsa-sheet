bdcbe79c3e4d0c6146b95e949eafaa7d9d011520
CodeSync <codesync@users.noreply.github.com>
2026-09-25 11:47:57 +0530

    Solved: Median of 2 sorted arrays (TakeUForward) Sheets: None

--- CodeSync/TakeUForward/all/Median of 2 sorted arrays/solution.java
+++ CodeSync/TakeUForward/all/Median of 2 sorted arrays/solution.java
@@ -5,35 +5,20 @@
  * Language: Java
  * Difficulty: Medium
  * Topics: Prep, Explore, My Spaces, Dashboard, Prep Hub, DSA, SQL, Planly
- * Runtime: 0.178 ms
+ * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-09-24T20:49:16.860Z
+ * Synced: 2026-09-25T06:01:46.503Z
  */
 
-int i = 0, j = 0, k = 0;
+class Solution {
+    public double median(int[] arr1, int[] arr2) {
         int[] merge = new int[arr1.length + arr2.length];
+
+        int i = 0, j = 0, k = 0;
+
         while (i < arr1.length && j < arr2.length) {
-            if (arr1[i] <= arr2[j]) {
-                merge[k++] = arr1[i++];
-            } else {
-                merge[k++] = arr2[j++];
-            }
+            if (arr1[i] <= arr2[j]) merge[k++] = arr1[i++];
+            else merge[k++] = arr2[j++];
         }
 
-        while (i < arr1.length) {
-            merge[k++] = arr1[i++];
-        }
-
-        while (j < arr2.length) {
-            merge[k++] = arr2[j++];
-        }
-
-        int n = merge.length;
-
-        if (n % 2 == 1) {
-            return merge[n / 2];
-        }
-
-        return (merge[n / 2 - 1] + merge[n / 2]) / 2.0;
-    }
-}
+        while (i < arr1.length) merge[k++] = arr1[i++];
