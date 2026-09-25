39b61b6849b4b53e848abc845c9087685bd0ebed
CodeSync <codesync@users.noreply.github.com>
2026-09-25 22:12:59 +0530

    Solved: 1096. Brace Expansion II (LeetCode) Sheets: None

---
+++ CodeSync/LeetCode/hard/all/1096. Brace Expansion II/solution.c
@@ -0,0 +1,14 @@
+/*
+ * Platform: LeetCode
+ * Problem: 1096. Brace Expansion II
+ * URL: https://leetcode.com/problems/brace-expansion-ii/description/?envType=daily-question&envId=2026-09-25
+ * Language: C
+ * Difficulty: Hard
+ * Topics: Hash Table, String, Backtracking, Stack, Breadth-First Search, Sorting
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-09-25T12:49:29.790Z
+ */
+
+Input: expression = "{a,b}{c,{d,e}}"
+Output: ["ac","ad","ae","bc","bd","be"]
