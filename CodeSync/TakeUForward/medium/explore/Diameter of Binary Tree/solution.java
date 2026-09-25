678d2fca50458efb63f54a6892ceaf27bbfd1aa2
CodeSync <codesync@users.noreply.github.com>
2026-09-25 13:24:23 +0530

    Solved: Diameter of Binary Tree (TakeUForward) Sheets: Grind75, Grind169, NeetCode150, Top100Liked, StriverA2Z, custom:algomaster-300, custom:arsh-goyal-dsa-sheet, custom:fraz-dsa-sheet, custom:neetcode-dsa-sheet, custom:striver-a2z-dsa-sheet, custom:striver-sde-sheet, custom:Rohit-Negi_DSA_Sheet, custom:love-babbar-dsa-sheet, custom:rohit-negi_dsa_sheet, custom:shradha-khapra-dsa-sheet

---
+++ CodeSync/TakeUForward/medium/explore/Diameter of Binary Tree/solution.java
@@ -0,0 +1,35 @@
+/*
+ * Platform: TakeUForward
+ * Problem: Diameter of Binary Tree
+ * URL: https://takeuforward.org/practice/dsa/diameter-of-binary-tree
+ * Language: Java
+ * Difficulty: Medium
+ * Topics: Prep, Explore, My Spaces, Dashboard, Prep Hub, DSA, SQL, Planly
+ * Runtime: 0.794 ms
+ * Memory: N/A
+ * Synced: 2026-09-25T06:11:13.934Z
+ */
+
+class Solution {
+    int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter;
+    }
+    private int height(TreeNode root) {
+        // Base case
+        if (root == null) return 0;
+        // Left subtree ki height
+        int left = height(root.left);
+
+        // Right subtree ki height
+        int right = height(root.right);
+
+        // Is node ke through diameter
+        diameter = Math.max(diameter, left + right);
+
+        // Current node ki height
+        return 1 + Math.max(left, right);
+    }
+}
